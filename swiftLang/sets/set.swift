8595d6e07d6c5843277fb0073dbe35792749f758
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-06 22:05:53 +0530

    add sets

---
+++ swiftLang/sets/set.swift
@@ -0,0 +1,10 @@
+var challenges : Set<String> = [
+    "get the flag",
+    "fold the flag",
+    "take a stones",
+    "fold the flag",
+    "get to the Home"
+] 
+print(challenges)
+print(challenges.contains("get the flag"))
+
