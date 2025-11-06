0ad1596ddd7f5067326386b929425e4e67560371
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-06 20:14:45 +0530

    add string

--- swiftLang/MyPlayground.playground/Contents.swift
+++ swiftLang/MyPlayground.playground/Contents.swift
@@ -1,11 +1,54 @@
 // var and const
-
+/*
 var username: String = "kishan";
 username = ""
-let accountId = 1234;
+let accountId = 1234; // constant
 
 //accountId = 123
 
 var numberOfLoginInADay: Int = 34
 
 print(numberOfLoginInADay)
+
+var name,email, address : String;
+name = ""
+email = "12"
+address = "21"
+
+*/
+
+// operators and range
+// var result = 4+4
+// print(result)
+// print(result == 1,"","")
+
+// // range
+// var myRange = 1...6
+
+// print(myRange)
+
+//for(i=){
+//    print(i)
+//}
+
+// var myStr = ""
+// myStr += "kishan"
+// myStr += " rana ghosh"
+// print(myStr)
+// myStr = "i am \(myStr)"
+// print(myStr)
+// print(myStr.uppercased())
+// print(myStr.contains("k"))
+
+// print(myStr.isEmpty)
+
+
+// type conversion
+
+let googleStock = 12.5
+let userStockBuy:Int = 1 + Int(googleStock)
+print(userStockBuy)
+
+let userAge = "55"
+let intVal = Int(userAge)
+print((intVal ?? 0) + 12)
