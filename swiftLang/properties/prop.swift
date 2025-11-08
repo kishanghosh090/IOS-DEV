a2df8741854feaea90baaf99958e22ff9d2287b1
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-08 21:57:11 +0530

    add class properties

--- swiftLang/properties/prop.swift
+++ swiftLang/properties/prop.swift
@@ -20,3 +20,45 @@ print(comic.description())
 comic.pages += 20
 print("Updated pages: \(comic.pages)")
 
+
+// lazy stored property example
+class ImageLoader {
+    var name: String
+    lazy var imageData: String = {
+        print("Loading image data for \(self.name)...") // This will only print when imageData is accessed the first time
+        // Simulate loading image data
+        return "ImageData for \(self.name)"
+    }()
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let loader = ImageLoader(name: "SampleImage")
+print("ImageLoader created.")
+print(loader.imageData)  // imageData is loaded here
+print(loader.imageData)  // imageData is already loaded, no print from closure
+
+// computed property example
+class Circle {
+    var radius: Double
+
+    init(radius: Double = 1.0) {
+        self.radius = radius
+    }
+    var area: Double {
+        get {
+            return Double.pi * radius * radius
+        }
+        set(newArea) {
+            radius = (newArea / Double.pi).squareRoot()
+        }
+    }
+
+  
+}
+let circle = Circle()
+// print("Area of circle: \(circle.area)")
+circle.area = 50.0
+print("Updated radius after setting area to 50.0: \(circle.radius)")
+// Computed property with getter and setter
