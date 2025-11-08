cc3a42c1b23ee367aac9e27c27a5a0cd8dfc760e
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-08 19:28:18 +0530

    add closure

--- swiftLang/closure/cl.swift
+++ swiftLang/closure/cl.swift
@@ -5,49 +5,62 @@
     
 // }
 
-var myClosure: () -> () = {} 
+// var myClosure: () -> () = {} 
 
-var numClosure: (Int) -> (Int) = { 
-    (a: Int) -> Int in
-    return a + 1
+// var numClosure: (Int) -> (Int) = { 
+//     (a: Int) -> Int in
+//     return a + 1
+// }
+
+// var val = 1
+
+// print("numClosure(5):", numClosure(val))
+// print(val)
+
+// var anotherNumClosure = {myNum in
+//     return myNum * 2
+// }
+// print("anotherNumClosure(5):", anotherNumClosure(val))// closures is used to pass around functionality as first class citizens
+// var founded = {()-> Int in
+//     return 42
+// }
+// print("founded():", founded())
+// var multiplyClosure = { (a: Int, b: Int) -> Int in
+//     return a * b
+// }
+// print("multiplyClosure(3,4):", multiplyClosure(3,4))
+// var greetClosure = { (name: String) -> String in
+//     return "Hello, \(name)!"
+// }
+// print(greetClosure("Alice"))
+// swapArr.swapAt(0, 2)
+// print("swapArr after swapAt(0,2):", swapArr)
+
+// var anotherFounders = { (names: [String]) -> [String] in
+//     return names.sorted()
+// }
+// let foundersList = ["Steve", "Bill", "Larry", "Elon"]
+// print("anotherFounders(foundersList):", anotherFounders(foundersList))
+// var swapArr = [1, 2, 3, 4, 5]
+// print("swapArr before swapAt(0,2):", swapArr)
+// var swapClosure = { (arr: inout [Int], index1: Int, index2: Int) -> () in
+//     let temp = arr[index1]
+//     arr[index1] = arr[index2]
+//     arr[index2] = temp
+// }
+// swapClosure(&swapArr, 0, 2)
+// print("swapArr after swapClosure(0,2):", swapArr)
+
+// func calculate(num: Int) -> Int {
+//     return num * num
+// }
+
+func calculate(num1: Int, num2: Int) -> Int {
+    return num1 * num1 + num2 * num2
 }
 
-var val = 1
-
-print("numClosure(5):", numClosure(val))
-print(val)
-
-var anotherNumClosure = {myNum in
-    return myNum * 2
+func sumSqr(nums1: Int, nums2: Int, closureParams: (Int,Int) -> Int) -> Int {
+    return closureParams(nums1, nums2) // we are calling the closure here when we pass it as parameter no need to use 'in' keyword and we just call it like a normal function without labels of parameters of closure for example here we just use nums1 and nums2 directly 
 }
-print("anotherNumClosure(5):", anotherNumClosure(val))// closures is used to pass around functionality as first class citizens
-var founded = {()-> Int in
-    return 42
-}
-print("founded():", founded())
-var multiplyClosure = { (a: Int, b: Int) -> Int in
-    return a * b
-}
-print("multiplyClosure(3,4):", multiplyClosure(3,4))
-var greetClosure = { (name: String) -> String in
-    return "Hello, \(name)!"
-}
-print(greetClosure("Alice"))
-swapArr.swapAt(0, 2)
-print("swapArr after swapAt(0,2):", swapArr)
-
-var anotherFounders = { (names: [String]) -> [String] in
-    return names.sorted()
-}
-let foundersList = ["Steve", "Bill", "Larry", "Elon"]
-print("anotherFounders(foundersList):", anotherFounders(foundersList))
-var swapArr = [1, 2, 3, 4, 5]
-print("swapArr before swapAt(0,2):", swapArr)
-var swapClosure = { (arr: inout [Int], index1: Int, index2: Int) -> () in
-    let temp = arr[index1]
-    arr[index1] = arr[index2]
-    arr[index2] = temp
-}
-swapClosure(&swapArr, 0, 2)
-print("swapArr after swapClosure(0,2):", swapArr)
-
+let result = sumSqr(nums1: 3, nums2: 4, closureParams: calculate) //when we pass function as parameter we don't use () and just use the name of function
+print("sumSqr with calculate closure:", result)
