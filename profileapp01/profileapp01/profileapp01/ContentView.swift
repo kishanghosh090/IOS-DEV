f6a5c7f4fa543189d38e48b538b693154561fd06
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-10 19:27:42 +0530

    add first profile app

---
+++ profileapp01/profileapp01/profileapp01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  profileapp01
+//
+//  Created by kishan rana ghosh on 09/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, from chai code chai aur kishan")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
