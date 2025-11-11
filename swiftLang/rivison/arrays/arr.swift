8a0fe63e3bdc69f50ded3be317fdd4e8f22f0871
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-11 23:18:53 +0530

    add arrays

--- swiftLang/rivison/arrays/arr.swift
+++ swiftLang/rivison/arrays/arr.swift
@@ -1,20 +1,59 @@
-import Foundation
-let oldArray = NSMutableArray(
-    array: [
-        "Anna",
-        "Alex",
-        "Brian",
-        "Jack"
-    ]
-)
+// import Foundation
+// let oldArray = NSMutableArray(
+//     array: [
+//         "Anna",
+//         "Alex",
+//         "Brian",
+//         "Jack"
+//     ]
+// )
 
-oldArray.add("Mary") // valid
-print(oldArray)
+// oldArray.add("Mary") // valid
+// print(oldArray)
 
-func changeTheArray(_ array: NSArray) {
-    let copy = array as! NSMutableArray // it is possible to change the copy of NSArray
-    copy.add("John") // it changes the orginal array as well because NSMutableArray is reference type
-    print("Inside function: \(copy)")
+// func changeTheArray(_ array: NSArray) {
+//     let copy = array as! NSMutableArray // it is possible to change the copy of NSArray
+//     copy.add("John") // it changes the orginal array as well because NSMutableArray is reference type
+//     print("Inside function: \(copy)")
+// }
+// changeTheArray(oldArray)
+// print(oldArray)
+
+
+// let objcArray: NSMutableArray = [1, 2, 3]
+// let swiftArray = objcArray as! [Int]  // convert to Swift Array
+
+
+// var arr: [String] = ["A", "B", "C"]
+
+// // swiftArray is mutable, 
+
+// arr.append("D") // valid
+// arr.append(contentsOf: ["Shakia", "William"]) // it is used to add multiple elements at once
+
+// print(arr)
+
+
+
+
+class IntegerReference {
+    var value = 10
 }
-changeTheArray(oldArray)
-print(oldArray)
+var firstIntegers = [IntegerReference(), IntegerReference()]
+var secondIntegers = firstIntegers
+
+
+// Modifications to an instance are visible from either array
+firstIntegers[0].value = 100
+// print(secondIntegers[0].value)
+// Prints "100"
+
+
+// Replacements, additions, and removals are still visible
+// only in the modified array
+firstIntegers[0] = IntegerReference()
+secondIntegers = firstIntegers
+print(firstIntegers[0].value)
+// Prints "10"
+print(secondIntegers[0].value)
+// Prints "100"
